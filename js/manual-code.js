43c92a0a61517909551c96b159d41a1b9b46326f
Amanda Brito <112962708+amandalbrito@users.noreply.github.com>
2025-07-16 19:34:43 -0300

    Update manual-code.js

--- js/manual-code.js
+++ js/manual-code.js
@@ -1,3 +1,5 @@
+import API_URL from "apiConfig.js";
+
 document.addEventListener('DOMContentLoaded', () => {
   const btnVoltar = document.getElementById('btnVoltar');
   const inputCodigoManual = document.getElementById('inputCodigoManual');
