b12c454b58796689ed78ffd1b62f630f28c7d5d5
Amanda Brito <112962708+amandalbrito@users.noreply.github.com>
2025-07-16 19:59:54 -0300

    Update apiConfig.js

--- js/apiConfig.js
+++ js/apiConfig.js
@@ -1,3 +1,3 @@
 // apiConfig.js
-const API_BASE_URL = "backend-javasemestral.railway.internal";
+const API_BASE_URL = "https://backend-javasemestral-production.up.railway.app/";
 export default API_BASE_URL;
