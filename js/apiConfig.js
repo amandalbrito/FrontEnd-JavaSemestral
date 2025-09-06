87483c8de45404f265b7b7218274f2ee244a5569
amandalbrito <amandaluciabrito73@gmail.com.br>
2025-09-06 19:10:56 -0300

    Mensagem de upload

--- js/apiConfig.js
+++ js/apiConfig.js
@@ -1,3 +1,3 @@
 // apiConfig.js
-const API_BASE_URL = "https://backend-javasemestral.onrender.com/js/apiConfig.js";
+const API_BASE_URL = "https://backend-javasemestral.onrender.com/api";
 export default API_BASE_URL;
